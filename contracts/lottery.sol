52e6a7636c41abfb290743c4858389cdd3bca6b0
KOBEsphinx242 <ado.coneo@gmail.com>
2021-09-02 17:30:00 -0500

    Add Pick Winner Method to Lottery Contract

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -8,10 +8,24 @@ pragma solidity >=0.5.0 <0.9.0;
  */
 contract Lottery {
     address public manager;
-    address[] public players;
+    address payable[] public players;
     
     constructor() {
         manager = msg.sender;
+    }
+    
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(payable(msg.sender));
+    }
+    
+    function random() private view returns (uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+    
+    function pickWinner() public {
+        uint index = random() % players.length;
+        players[index].transfer(address(this).balance);
     }
    
     
